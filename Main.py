708f7f04acd1df279fa6acfb628e0284562bcbf9
Vitor Almeida <tikalleu2005843@gmail.com>
2025-07-16 21:42:18 -0300

    add arguments

--- Main.py
+++ Main.py
@@ -1,13 +1,22 @@
 import socket, sys, argparse
 
-
-def argument():
+args_list = []
+def init_program():
     parse = argparse.ArgumentParser()
+    parse.add_argument('-d', '--domin', help="which domain to test")
     parse.add_argument('-p', '--port', help="specify which port you want to scan")
-    parse.add_argument('-hs', help="test")
-
+    parse.add_argument('-c', '--comun', action="store_true", help="tests all the most common ports")
+    parse.add_argument('-f', '--first', action='store_true', help="test wing first 1000 doors")
     argv = parse.parse_args()
-    print(argv.port)
+
+    if argv.domin:
+        args_list.append(argv.domin)
+    
+
+print(args_list)   
+    
 
 
-argument()
+
+init_program()
+
