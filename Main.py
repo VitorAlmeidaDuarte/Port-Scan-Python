64054b58a816ad79440fe290c344600ada966a02
Vitor Almeida <tikalleu2005843@gmail.com>
2025-07-19 22:55:45 -0300

    final script

--- Main.py
+++ Main.py
@@ -4,10 +4,8 @@ def connect_port(target, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.5)
     if client.connect_ex((target, port)) == 0:
-        return  print(f'Aberta: {port}')
+        return  print(f'open: {port}')
 
-  
-    
 
 
 def define_arguments():
@@ -19,14 +17,11 @@ def define_arguments():
     parse.add_argument('-f', '--first', action='store_true', help="test wing first 10000 doors")
     argv = parse.parse_args()
 
-    if argv.domin:
-        args_list['domin'] = argv.domin
-    if argv.port:
-        args_list['port'] = argv.port
-    if argv.common:
-        args_list['common'] = argv.common
-    if argv.first:
-        args_list['first'] = argv.first
+
+    args_list['domin'] = argv.domin
+    args_list['port'] = argv.port
+    args_list['common'] = argv.common
+    args_list['first'] = argv.first
 
     return args_list
     
@@ -34,11 +29,18 @@ ports_common = [20,21,22,25,53,80,123,179,443,500,587,3389]
 
 args_def = define_arguments()
 
+
 if args_def['common'] == True:
     for p in ports_common:
-        port = connect_port(args_def['domin'], p)
+        connect_port(args_def['domin'], p)
 
 
+if args_def['first'] == True:
+    for p in range(1, 10000):
+        connect_port(args_def['domin'], p)
+
+if args_def['port']:
+    connect_port(args_def['domin'], int(args_def['port']))
 
 
 
